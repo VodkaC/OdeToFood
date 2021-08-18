4e3b210d7db3d9142a2b6ab329b0556d77e9b66e
Igor Nicsenko <nichena7@gmail.com>
2021-08-18 02:09:03 +0200

    Create in Data new Class Interface and MemoryData

---
+++ OdeToFood.Data/Service/InMemoryRestaurantData.cs
@@ -0,0 +1,14 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data.Service
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
