1905ab7c16f1c3240459b74cbdce2b0bcf0b023f
Igor Nicsenko <nichena7@gmail.com>
2021-08-27 00:17:51 +0200

    Create Greetign View, ViewModel, Controll. Add new string in WebConfig

---
+++ OdeToFood.Web/Controllers/RestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestController : Controller
+    {
+        // GET: Rest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Rest2()
+        {
+            return View();
+        }
+
+    }
+}
